47e64497c23925d99d10db57b50eb850ec2d5664
Holden Hirsch <hirsch.holden@gmail.com>
2024-06-02 13:03:12 -0400

    test

---
+++ slider.py
@@ -0,0 +1,40 @@
+<!-- Spacer Section - ->
+                        <div class = "h-20" > </div >
+                        <!-- Slider main container - ->
+                        <div class = "swiper brand-slider jos jos-fade swiper-initialized swiper-horizontal swiper-backface-hidden" data-jos_animation = "fade" data-jos_once = "1" data-jos_counter = "0" data-jos_scrolldirection = "down" >
+                            <!-- Additional required wrapper - ->
+                            <div class = "swiper-wrapper" id = "swiper-wrapper-fe4e0fcd90df4f3e" aria-live = "off" style = "transition-duration: 0ms; transform: translate3d(-1068.8px, 0px, 0px); transition-delay: 0ms;" >
+                                <!-- Slides - ->
+                                <div class = "swiper-slide" role = "group" aria-label = "1 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-1.png" alt = "brand-1" width = "180" height = "38" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide" role = "group" aria-label = "2 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-2.png" alt = "brand-2" width = "183" height = "35" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide" role = "group" aria-label = "3 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-3.png" alt = "brand-3" width = "172" height = "35" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide swiper-slide-prev" role = "group" aria-label = "4 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-4.png" alt = "brand-4" width = "175" height = "30" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide swiper-slide-active" role = "group" aria-label = "5 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-5.png" alt = "brand-5" width = "168" height = "36" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide swiper-slide-next" role = "group" aria-label = "6 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-1.png" alt = "brand-1" width = "180" height = "38" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide" role = "group" aria-label = "7 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-2.png" alt = "brand-2" width = "183" height = "35" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide" role = "group" aria-label = "8 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-3.png" alt = "brand-3" width = "172" height = "35" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide" role = "group" aria-label = "9 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-4.png" alt = "brand-4" width = "175" height = "30" class = "max-w-full" >
+                                </div >
+                                <div class = "swiper-slide" role = "group" aria-label = "10 / 10" style = "width: 162.2px; margin-right: 105px;" >
+                                    <img src = "assets/img/th-1/brand-5.png" alt = "brand-5" width = "168" height = "36" class = "max-w-full" >
+                                </div >
+                            </div >
+                        <span class = "swiper-notification" aria-live = "assertive" aria-atomic = "true" > </span > </div >
+                    </div>
